9a1ad243016a5a1a9ebc2e12376a830e983536ef
mgiffu <murilogiffu89@gmail.com>
2025-04-29 21:52:31 -0300

    Creating the Function "addItem" to add new items to the list

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Selecioanando elementos do DOM
 const form = document.querySelector('form'); // utilizado para evento de envio do formulário
 const input = document.getElementById('add-item'); // utilizado para a função de de um novo item na lista
+const itemsList = document.getElementById("items-list")
+const alert = document.getElementById("alert")
+const checkboxAlert = document.getElementById("checkbox-alert")
 
 // Evento de envio de formulário
 form.addEventListener("submit", (event) => {
@@ -14,4 +17,29 @@ form.addEventListener("submit", (event) => {
 )
 
 // Função para adicionar um novo item na lista
-function addItem(text) {}
+function addItem(text) {
+  const template = document.getElementById("item-template"); // Obtém o template com a id "item-template"
+  const divClone = template.content.cloneNode(true).firstElementChild; // Clona o template e obtém o primeiro elemento filho
+
+  const uniqueId = "item-" + Date.now(); // Gera um id único com base na data e hora atual
+
+  const checkbox = divClone.querySelector("input[type=checkbox]")
+  checkbox.id = uniqueId // Define o id do checkbox com o id único
+  checkbox.name = uniqueId // Define o nome do checkbox com o id único
+
+  const label = divClone.querySelector("label")
+  label.setAttribute("for", uniqueId) // Define o atributo "for" do label com o id único
+
+  divClone.querySelector(".item-text").textContent = text // Define o texto digitado do item com o valor do input
+
+  const removeButton = divClone.querySelector(".remove-button") // Obtém o botão de remoção
+  removeButton.addEventListerner("click", () => {
+    if (checkbox.checked) {
+      divClone.remove() // remove o item
+      showRemovalAlert() // mostra alerta de remoção do item
+    } else {
+      showCheckboxRiqueridAlert() // mostra alerta para marcar o item antes de excluí-lo
+    }
+  })
+}
+
