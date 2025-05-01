f7b6506561e21a222db49a827c8b1c6cd2061fd4
mgiffu <murilogiffu89@gmail.com>
2025-05-01 10:53:23 -0300

    Selecting DOM elements

---
+++ script.js
@@ -0,0 +1,6 @@
+// Selecionando elementos do DOM
+const form = document.querySelector("form")
+const input = document.getElementById("add-item")
+const itemsList = document.getElementById("items-list")
+const alert = document.getElementById("alert")
+const checkbosAlert = document.getElementById("checkbox-alert")
