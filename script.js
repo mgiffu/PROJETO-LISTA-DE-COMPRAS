1480c0aa95db26d9371e8722481534f6772a5c76
mgiffu <murilogiffu89@gmail.com>
2025-05-01 10:55:12 -0300

    Hiding "alert" and "checkboxAlert"

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const form = document.querySelector("form")
 const input = document.getElementById("add-item")
 const itemsList = document.getElementById("items-list")
 const alert = document.getElementById("alert")
-const checkbosAlert = document.getElementById("checkbox-alert")
+const checkboxAlert = document.getElementById("checkbox-alert")
+
+// Escondendo os avisos de alerta
+alert.style.display = "none"
+checkboxAlert.style.display = "none"
