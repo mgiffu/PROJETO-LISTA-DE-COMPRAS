1ec737d1d109b47bed4242cc80a9c5e7b9c66c3c
mgiffu <murilogiffu89@gmail.com>
2025-05-01 11:06:36 -0300

    Creating a form submission event

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ const checkboxAlert = document.getElementById("checkbox-alert")
 // Escondendo os avisos de alerta
 alert.style.display = "none"
 checkboxAlert.style.display = "none"
+
+// Adicionando evento de envio de formulário
+form.addEventListener("submit", (event) => {
+  event.preventDefault() // removendo efeito padrão de recarregar a página
+
+  const itemText = input.value.trim() // pegar o valor do imput e remover epaços do inicio e fim
+
+  if (itemText !== "") { // se o valor do input for diferente de vazio
+    addItem(itemText) // cria um novo item o com valor do input
+    input.value = "" // limpa o valor do input adicionado
+  }
+})
+
+
